8988544401c259ddb15b42d0697b027c459f3711
IDilantha <isurudilantha@gmail.com>
2020-01-16 11:40:12 +0530

    Create transactionManager bean method in JPAConfig file

--- src/main/java/io/github/idilantha/pos/JPAConfig.java
+++ src/main/java/io/github/idilantha/pos/JPAConfig.java
@@ -7,11 +7,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 
@@ -51,7 +55,10 @@ public class JPAConfig {
         return hjva;
     }
 
-
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManager){
+        return new JpaTransactionManager(entityManager);
+    }
 
 
 }
